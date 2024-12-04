68c6f1a683e2cd6cabf35321ae197ebbe4eff0dd
e.akhantev <e.akhantev@s7.ru>
2024-12-05 01:21:32 +0400

    feat: add prettier settings

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,8 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import prettierPlugin from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
@@ -16,9 +18,27 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'prettier': prettierPlugin
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      ...eslintConfigPrettier.rules,
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'no-undef': 'error',
+      'no-unused-vars': [
+        'error',
+        {
+          'vars': 'all',
+          'args': 'none'
+        }
+      ],
+      'no-use-before-define': 'off',
+      'no-this-before-super': 'off',
+      'no-undef-init': 'off',
+      'prettier/prettier': 'error',
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
